df17d343f697e359cce7f6d821c953f123f59297
evonnn16 <74967730+evonnn16@users.noreply.github.com>
2020-11-26 14:50:30 +0800

    Add files via upload

---
+++ demoscript.js
@@ -0,0 +1,11 @@
+function myFunction() 
+{document.getElementById("demo").innerHTML = "Paragraph changed.";}  
+
+document.getElementById("demo2").innerHTML = 5 + 6;
+
+var x, y, z;
+x = 10;
+y = 12;
+z = x + y * 2;
+document.getElementById("demo3").innerHTML =
+"The value of z is " + z + ".";
